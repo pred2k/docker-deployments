6cc2eaef8cbe8f206a95f76f5fa3e29c4cc71a8e
pred2k <basti_services@tutanota.com>
2021-02-28 15:21:20 +0100

    added script to update images and restart containers

---
+++ update-images-and-restart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+BASE_DIR='/docker-deployments'
+
+echo -e "Update libreddit image and restart container\n"
+cd ${BASE_DIR}/libreddit && \
+docker-compose pull && docker-compose up --detach --force-recreate --remove-orphans
+echo -e "libreddit update done\n"
+echo "Now cleanup old images and volumes"
+docker system prune --volumes --force
