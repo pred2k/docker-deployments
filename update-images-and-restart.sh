9829475df3d4f99ad4abca0718256b83d2e34610
pred2k <basti_services@tutanota.com>
2021-03-07 15:27:48 +0100

    added whoogle to update&restart script

--- update-images-and-restart.sh
+++ update-images-and-restart.sh
@@ -5,5 +5,11 @@ echo -e "Update libreddit image and restart container\n"
 cd ${BASE_DIR}/libreddit && \
 docker-compose pull && docker-compose up --detach --force-recreate --remove-orphans
 echo -e "libreddit update done\n"
+
+echo -e "Update whoogle image and restart container\n"
+cd ${BASE_DIR}/whoogle && \
+docker-compose pull && docker-compose up --detach --force-recreate --remove-orphans
+echo -e "whoogle update done\n"
+
 echo "Now cleanup old images and volumes"
 docker system prune --volumes --force
