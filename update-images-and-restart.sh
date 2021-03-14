c4d7498f09a3826c26bb07b1fbc8046284fde540
pred2k <basti_services@tutanota.com>
2021-03-14 19:29:00 +0100

    removed style.css and fixed docker run in update&restart script

--- update-images-and-restart.sh
+++ update-images-and-restart.sh
@@ -25,7 +25,7 @@ echo "Now cleanup old images and volumes:"
 docker system prune --volumes --force
 
 echo -e "Configure Whoogle via curl:"
-docker run -it --rm --network traefik_default curlimages/curl \
+docker run --rm --network traefik_default curlimages/curl \
   whoogle-search:5000/config \
   -d 'lang_interface=lang_en&lang_search=lang_en&alts=on' \
   --retry 5 --retry-delay 10 --retry-connrefused
