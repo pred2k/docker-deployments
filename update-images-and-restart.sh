1d6a93bedf807327061c364845551b73b96e213f
pred2k <does.not@matter.com>
2022-03-12 09:42:33 +0100

    added security.txt to Caddyfile

--- update-images-and-restart.sh
+++ update-images-and-restart.sh
@@ -24,8 +24,3 @@ echo -e "whoogle update done\n"
 echo "Now cleanup old images and volumes:"
 docker system prune --volumes --force
 
-echo -e "Configure Whoogle via curl:"
-docker run --rm --network traefik_default curlimages/curl \
-  whoogle-search:5000/config \
-  -d 'lang_interface=lang_en&lang_search=lang_en&alts=on' \
-  --retry 5 --retry-delay 10 --retry-connrefused
